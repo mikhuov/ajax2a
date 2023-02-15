9d147d5302af67132e5ba8fb5aa212fe255fd6e0
mikhuov <mikhuovinen@gmail.com>
2023-02-15 22:46:13 +0200

    Added: Progress

--- index.js
+++ index.js
@@ -40,16 +40,28 @@ function loadDoc() {
 function generateTable(xml) {
     var i;
     var xmlDoc = xml.responseXML;
-    var table = "<tr><th>Title</th><th>Theatre</th><th>Link</th></tr>";
+    var table = "<tr><th>Title</th><th>Theatre</th><th>Starts</th><th></th></tr>";
     var collection = xmlDoc.getElementsByTagName("Show");
     
     console.log(collection);
     for (i = 0; i < collection.length; i++) {
+        var date = new Date(collection[i].getElementsByTagName("dttmShowStart")[0].childNodes[0].nodeValue);
+
+        let day = date.getDate();
+        let month = date.getMonth();
+        month += 1;
+        let year = date.getFullYear();
+        let hour = date.getHours();
+        let minute = date.getMinutes();
+        let newDate = day + "." + month + "." + year + ", " + hour + ":" + minute;
 
         table += "<tr><td>" +
         collection[i].getElementsByTagName("Title")[0].childNodes[0].nodeValue +
         "</td><td>" + collection[i].getElementsByTagName("Theatre")[0].childNodes[0].nodeValue +
-        `</td><td> <a href=${collection[i].getElementsByTagName("EventURL")[0].childNodes[0].nodeValue}>LINK</a>` +
+        `</td><td> ${newDate}` +  
+        `</td><td> <a href=${collection[i].getElementsByTagName("EventURL")[0].childNodes[0].nodeValue}>
+                    <img src=${collection[i].getElementsByTagName("EventSmallImagePortrait")[0].childNodes[0].nodeValue}>
+                    </a>` +
         "</td></tr>";
       }
       document.getElementById("xmlTable").innerHTML = table;
@@ -71,13 +83,13 @@ function populateDropdown(xml) {
 
 function onChange() {
     var value = document.getElementById("theatreSelect").value;
-    console.log('VALUE: ', value);
+    var date = new Date("ddmmyyyy");
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             generateTable(xhttp);
         }
       };
-    xhttp.open("GET", `https://www.finnkino.fi/xml/Schedule/?area=${value}&dt=15.02.2023`, true);
+    xhttp.open("GET", `https://www.finnkino.fi/xml/Schedule/?area=${value}&dt=${date}`, true);
     xhttp.send();  
 }
