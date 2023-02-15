6c19a1e6c9709f2010a0703c5bba04f170e839dc
mikhuov <mikhuovinen@gmail.com>
2023-02-15 17:18:10 +0200

    Added: Progress

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
-const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=4934e2d5";
+/* const API_URL = "http://www.omdbapi.com/?i=tt1849352&apikey=4934e2d5";
+var data = {};
 
-fetch(API_URL).then(function(response) {
-    console.log(response.json());
-    return response.json();
-}).catch(function(error) {
-    console.log(error);
-});
+function fetchData() {
+    fetch(API_URL).then(response => {
+        console.log(response.json());
+        return response.json();
+    }).catch(function(error) {
+        console.log(error);
+    });
+}
+
+fetchData(); */
+
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            generateTable(xhttp);
+        }
+      };
+    xhttp.open("GET", "https://www.finnkino.fi/xml/Schedule/", true);
+    xhttp.send();
+  }
+
+function generateTable(xml) {
+    var i;
+    var xmlDoc = xml.responseXML;
+    var table = "<tr><th>Title</th><th>Theatre</th><th>Link</th></tr>";
+    var collection = xmlDoc.getElementsByTagName("Show");
+    console.log(collection);
+    for (i = 0; i < collection.length; i++) {
+        table += "<tr><td>" +
+        collection[i].getElementsByTagName("Title")[0].childNodes[0].nodeValue +
+        "</td><td>" +
+        collection[i].getElementsByTagName("Theatre")[0].childNodes[0].nodeValue +
+        "</td><td>" +
+        collection[i].getElementsByTagName("EventURL")[0].childNodes[0].nodeValue +
+        "</td></tr>";
+      }
+      document.getElementById("xmlTable").innerHTML = table;
+}
+loadDoc();
